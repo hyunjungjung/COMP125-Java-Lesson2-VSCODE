abe3e7dbda2f8f5f215169d554426f72893c3fc9
hyunjungjung <hjung3@my.centennialcollege.ca>
2016-05-16 10:18:02 -0400

    Added simple codeto app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -5,7 +5,12 @@
     "use strict";
     
     
- //Code codes in here   
+    //Code codes in here   
     console.log("App Started...")
+    
+    // Create a reference to a p element with an id of "firstParagarph"
+    var firstParagarph = document.getElementById("firstParagraph");
+  
+    firstParagarph.textContent = "It's alive!!!"
     
 })();
