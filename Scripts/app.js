d9e80aa2060f67e1fbdfd0124bfe6f56042f467b
hyunjungjung <hjung3@my.centennialcollege.ca>
2016-05-16 09:36:44 -0400

    Initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,11 @@
+/* Main JavaScript file */
+
+//IIFE - Immediately Invoked Functiona Expression
+(function(){
+    "use strict";
+    
+    
+ //Code codes in here   
+    console.log("App Started...")
+    
+})();
